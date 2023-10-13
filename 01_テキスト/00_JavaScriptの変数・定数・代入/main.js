4535d5f1eef6438f8dcbac53fae8f1784a7f2569
yuro <yuromiki0831@gmail.com>
2023-10-13 20:45:04 +0900

    コミット

--- 01_テキスト/00_JavaScriptの変数・定数・代入/main.js
+++ 01_テキスト/00_JavaScriptの変数・定数・代入/main.js
@@ -4,6 +4,11 @@ console.log(myNickname) // => "エキスパくん" と表示される
 
 // TRY: 変数 myNickname に再代入して自分のあだ名を表示してください
 
+myNickname = "ゆうろ"
+
 console.log(myNickname)
 
 // TRY: myAge を定数で宣言して自分の年齢を表示してください
+
+const myAge = "21"
+console.log(myAge)
